9355558fac024b13c526d553d6735d2887c169cc
Artur Rieznik <artuar1990@gmail.com>
2019-06-26 00:01:44 +0300

    google auth

---
+++ README.md
@@ -0,0 +1,31 @@
+Usefull links:
+
+deploy:
+https://cloud.google.com/appengine/docs/standard/python3/quickstart
+
+
+storage:
+https://cloud.google.com/datastore/docs/quickstart
+https://cloud.google.com/datastore/docs/datastore-api-tutorial
+
+
+webAuth:
+https://developers.google.com/identity/sign-in/web/sign-in
+
+
+OAuth2 Authentication:
+https://developers.google.com/adwords/api/docs/guides/authentication#webapp
+
+
+Management:
+https://console.cloud.google.com/apis/credentials?project=webgl-site-constructor
+
+
+Articles of build configs:
+https://medium.com/google-cloud/continuous-delivery-in-google-cloud-platform-cloud-build-with-app-engine-8355d3a11ff5
+
+
+three.js 
+https://threejs.org/examples/#webgl_terrain_dynamic
+
+
--- front-end/src/app/app.component.ts
+++ front-end/src/app/app.component.ts
@@ -1,10 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+
+declare const gapi: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'front-end';
+export class AppComponent implements AfterViewInit {
+  title = 'webgl-site-constructor';
+
+  public onSignIn(googleUser) {
+    const profile = googleUser.getBasicProfile();
+    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+    console.log('Name: ' + profile.getName());
+    console.log('Image URL: ' + profile.getImageUrl());
+    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+
+    // get token 
+    const id_token = googleUser.getAuthResponse().id_token;
+
+    // send token to server 
+    const xhr = new XMLHttpRequest();
+    xhr.open('POST', '/tokensignin');
+    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    xhr.onload = () => {
+      console.log('Signed in as: ' + xhr.responseText);
+    };
+    xhr.send('token=' + id_token);
+  }
+
+  signOut() {
+    const auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(() => {
+      console.log('User signed out.');
+    });
+  }
+
+  ngAfterViewInit() {
+    gapi.signin2.render('my-signin2', {
+        scope: 'profile email',
+        width: 240,
+        height: 50,
+        longtitle: true,
+        theme: 'light',
+        onsuccess: param => this.onSignIn(param)
+    });
+}
 }
